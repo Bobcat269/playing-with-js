ee7fbe89a544215071f1fa37508ce1271c74072d
caleedev <caleedev@gmail.com>
2025-08-27 16:14:40 -0400

    branching refresher

--- app.js
+++ app.js
@@ -48,6 +48,24 @@ console.log(planetHasWater2('Pluto'));
 //Since the comparator is a binary boolean we can just return the result instead of specifying return true.
 //We can also us the OR || operator since we are listing all positive matches.
 
+//Reminder that we use === in JS for strict equality whereas == would convert types?
+//What would that look like....?
+
+function isThisATwo (x) {
+    return x == 2
+}
+console.log('Is this a two?');
+
+console.log(isThisATwo(2));
+console.log(isThisATwo('2'));
+
+//Versus....
+function aintThisATwo (x) {
+    return x === 2
+}
+console.log('Aint this a two?');
+console.log(aintThisATwo(2));
+console.log(aintThisATwo('2'));
 
 // Problem 2
 // Write a function named computeArea.
@@ -75,3 +93,46 @@ console.log(multiply(2,2));
 console.log(divide(2,2));
 
 
+
+//Make a stoplight to demonstrate else if branching
+
+function stoplight(color) {
+    if (color === 'Green') {
+        console.log('Time to go.');
+    } else if (color === 'Yellow') {
+        console.log('Time to slow.');
+    } else if (color === 'Red') {
+        console.log('It`s time to stop.  Where are your parents?');
+    } else {
+        console.log('Invalid color input.');   
+    }
+    
+}
+
+stoplight('Green')
+console.log(stoplight('Green'));
+console.log(stoplight('Yellow'));
+console.log(stoplight('Red'));
+
+//if we try to log the function here we also get the line "undefined"
+//This is likely because the log is being done in the function rather than returned?
+//not that it's necessary, but would this change if our branch statement was returning instead of logging?
+function stoplight2(color) {
+    if (color === 'Green') {
+        return 'Time to go.';
+    } else if (color === 'Yellow') {
+        return 'Time to slow.';
+    } else if (color === 'Red') {
+       return'It`s time to stop.  Where are your parents?';
+    } else {
+       return 'Invalid color input.';   
+    }
+    
+}
+
+//I feel like this is gonna mess up?  Fixed by changing return console.log('blah blah') to return 'blah blah'
+console.log(stoplight2('Green'));
+console.log(stoplight2('Yellow'));
+console.log(stoplight2('Red'));
+
+//Summary: function will return undefined unless an implicit return is provided.
